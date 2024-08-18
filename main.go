278975bc1d6afb977a79c0c53f2bbaf75490c263
Ian Bottomley <ian.bottomley@ricardo.com>
2024-08-18 09:13:07 +0100

    add form

--- main.go
+++ main.go
@@ -4,22 +4,55 @@ import (
 	"github.com/rivo/tview"
 )
 
-func main() {
+type Application struct {
+	app   *tview.Application
+	pages *tview.Pages
+	form  *tview.Form
+}
+
+func NewApplication() *Application {
 	app := tview.NewApplication()
 	pages := tview.NewPages()
-	mainMenu(app, pages)
-	if err := app.SetRoot(pages, true).EnableMouse(true).Run(); err != nil {
+	form := tview.NewForm()
+	return &Application{app: app, pages: pages, form: form}
+}
+
+func main() {
+	app := NewApplication()
+	mainMenu(app)
+	quickJournalEntry(app)
+	if err := app.app.SetRoot(app.pages, true).EnableMouse(true).Run(); err != nil {
 		panic(err)
 	}
 
 }
 
-func mainMenu(app *tview.Application, pages *tview.Pages) {
+func mainMenu(app *Application) {
 
 	list := tview.NewList().
-		AddItem("Option 1", "Option 1", '1', nil).
+		AddItem("Quick Journal Entry", "Add a quick entry to todays Journal", '1', func() {
+			switchToQuickJournalEntry(app)
+		}).
 		AddItem("Quit", "Press to exit", 'q', func() {
-			app.Stop()
+			app.app.Stop()
 		})
-	pages.AddPage("main", list, true, true)
+	app.pages.AddPage("main", list, true, true)
+}
+
+func switchToQuickJournalEntry(app *Application) {
+	app.form.Clear(true)
+
+	app.form.AddInputField("Title", "", 30, nil, nil).
+		AddInputField("Body", "", 30, nil, nil).
+		AddButton("Save", func() {
+			app.pages.SwitchToPage("main")
+		}).
+		AddButton("Quit", func() {
+			app.app.Stop()
+		})
+	app.pages.SwitchToPage("quickJournalEntry")
+}
+
+func quickJournalEntry(app *Application) {
+	app.pages.AddPage("quickJournalEntry", app.form, true, false)
 }
